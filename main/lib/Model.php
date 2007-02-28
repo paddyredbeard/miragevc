9b6142cc615b6a2ed1062a24c6910f733aecad4b
pbarabe <pbarabe@a6b614ad-652a-0410-8737-826cf792e4c8>
2007-02-28 00:46:07 +0000

    - Workin on data abstraction.
    git-svn-id: svn+ssh://svn.code.sf.net/p/miragevc/code@5 a6b614ad-652a-0410-8737-826cf792e4c8

--- main/lib/Model.php
+++ main/lib/Model.php
@@ -1,9 +1,83 @@
 <?php
 
-abstract class Model {
+abstract class Model extends MDB2 {
 
-    public function __construct() {}
-    public function __destruct() {}
+	public $data ;
+	public $schema ;
+	protected $table ;
+	protected $pkField ;
+	protected $dbConnection ;
+
+	public function __construct( $classFile=null, $recno=null ) {
+		$this->schema = Model::getSchema( $classFile ) ;
+		$this->dbConnection = &MDB2::singleton( APPLICATION_DSN ) ;
+
+
+		// some error checking on the connection
+		if( PEAR::isError( $this->dbConnection )) {
+			showDebug( array( 'Connection Error' => $this->dbConnection->getMessage() )) ;
+		}
+
+
+		// some error checking on the schema
+		if( PEAR::isError( $this->schema )) {
+			showDebug( array( 'Schema Error' => $this->schema->getMessage() )) ;
+		}
+
+
+		$this->table = $this->schema['dbparams']['table'] ;
+		$this->pkField = $this->schema['dbparams']['pkfield'] ;
+
+
+		// populate $this->data if a record number was passed
+		if( !empty( $recno )) {
+			$sql = "SELECT " ;
+			$i = 1 ;
+			foreach( $this->schema['fields'] as $key=>$nextField ) {
+				$sql .= $nextField ;
+
+				if( $i < count( $this->schema['fields'] )) {
+					$sql .= ", " ;
+				}
+				$i++ ;
+			}
+			$sql .= " FROM {$this->table} WHERE {$this->pkField}=$recno" ;
+
+			$queryResult = $this->dbConnection->query( $sql ) ;
+		
+			if( PEAR::isError( $queryResult )) {
+				showDebug( array( 'Query Error' => $queryResult->getMessage() )) ;
+			} else {
+				while( $row = $queryResult->fetchRow( MDB2_FETCHMODE_ASSOC )) {
+					foreach( $row as $field=>$value ) {
+						$this->data[$field] = $value ;
+					}
+				} 
+			}
+		}
+
+	}// end constructor
+
+
+	protected function getSchema( $aClassFile ) {
+		$output = false ;
+                $schemaFile = str_replace( ".php", ".ini", $aClassFile ) ;
+
+                return file_exists( $schemaFile ) ?
+	                parse_ini_file( $schemaFile, true ) :
+                	PEAR::raiseError(
+                                "No database schema file found for class: Tried \"$schemaFile\""
+                                ) ;
+	}// end getSchema
+
+
+	public function __destruct() {}
+
+
+        public function __toString() {
+                showDebug( $this->data ) ;
+        }
+
 
 }// end Model
 
