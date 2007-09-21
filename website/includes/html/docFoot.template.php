2f66137be86fa576aec7b3bbf3694822a9c7dfa7
spirituscanis <spirituscanis@a6b614ad-652a-0410-8737-826cf792e4c8>
2007-09-21 18:12:46 +0000

    - Removing google account info.
    git-svn-id: svn+ssh://svn.code.sf.net/p/miragevc/code@45 a6b614ad-652a-0410-8737-826cf792e4c8

--- website/includes/html/docFoot.template.php
+++ website/includes/html/docFoot.template.php
@@ -1,37 +1,4 @@
 		<div id='googleAdSense'>
-			<script type="text/javascript">
-				<!--
-				google_ad_client = "pub-3974726768428919";
-				google_ad_width = 120;
-				google_ad_height = 60;
-				google_ad_format = "120x60_as_rimg";
-				google_cpa_choice = "CAAQxZqazgEaCMOiwb9yonQWKIHD93M";
-				google_ad_channel = "";
-				//-->
-			</script>
-			<script type="text/javascript" 
-				src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-			</script>
-
-			<script type='text/javascript'>
-				<!--
-				google_ad_client = 'pub-3974726768428919';
-				google_ad_width = 468;
-				google_ad_height = 60;
-				google_ad_format = '468x60_as';
-				google_ad_type = 'text';
-				//2007-03-13: paddyworks
-				google_ad_channel = '3496330360';
-				google_color_border = 'FFFFFF';
-				google_color_bg = 'FFFFFF';
-				google_color_link = '333366';
-				google_color_text = '666666';
-				google_color_url = '333366';
-				//-->
-			</script>
-			<script type='text/javascript'
-				src='http://pagead2.googlesyndication.com/pagead/show_ads.js'>
-			</script>
 		</div>
 
 		<div id='foot'>
@@ -68,13 +35,5 @@
 		</div>
 
 		</div>
-
-		<!-- Google Analytics -->
-		<script src='http://www.google-analytics.com/urchin.js' type='text/javascript'>
-		</script>
-		<script type='text/javascript'>
-		_uacct = 'UA-410258-3';
-		urchinTracker();
-		</script>
 	</body>
 </html>
